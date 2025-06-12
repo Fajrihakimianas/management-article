3b1f0614d80f1f5b474e8bb8a95ee30e911b86ac
Fajrihakimianas <55306306+Fajrihakimianas@users.noreply.github.com>
2025-06-12 16:45:51 +0700

    Add articles layout and page components; update root layout styles

---
+++ app/articles/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Articles() {
+  return (
+    <div>
+      <div className="h-screen bg-red-200">Hero</div>
+      <div>tes</div>
+      <div>tes</div>
+    </div>
+  );
+}
